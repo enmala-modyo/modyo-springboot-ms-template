5f8b4c67a41b65343e65f331dca10350a4fffefc
Enrique Maldonado <emaldonado@modyo.com>
2023-01-24 09:03:23 -0300

    feat: upgrade template to Spring 2.7

--- {{ cookiecutter.project_name }}/src/main/java/{{ cookiecutter.package_dir }}/Application.java
+++ {{ cookiecutter.project_name }}/src/main/java/{{ cookiecutter.package_dir }}/Application.java
@@ -3,9 +3,11 @@ package {{ cookiecutter.group_id }};
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ComponentScan({"com.modyo.ms.commons", "{{ cookiecutter.group_id }}"})
+@EnlableWebMvc
 public class Application {
 
   public static void main(String[] args) {
